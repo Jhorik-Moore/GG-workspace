9d003694bdfb87d075c4f2dbac8f9a02ad4f9d23
Egor <72651998+Jhorik-Moore@users.noreply.github.com>
2021-10-08 17:42:03 +0300

    !MY_PASSWORD

--- src/main/java/com/admin/task/controllers/MyConstants.java
+++ src/main/java/com/admin/task/controllers/MyConstants.java
@@ -6,6 +6,6 @@ public class MyConstants {
     public static final String MY_EMAIL = "GGBanksender@gmail.com";
 
     // Replace password!!
-    public static final String MY_PASSWORD = "GGBanksender765567";
+    public static final String MY_PASSWORD = *******;
 
 }
